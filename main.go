e0399a608f7698af5d59a439468aa90546862f7c
wilbur ma <wilbur.ma@hotmail.com>
2014-06-15 20:47:51 +0800

    update xiami service

--- main.go
+++ main.go
@@ -1,10 +1,64 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "io/ioutil"
     "net/http"
+    "encoding/json"
 )
 
+type Song struct {
+    Name string `json:"song_title"`
+    Url string `json:"song_src"`
+    Author string `json:"song_author"`
+    Provider string `json:"song_provider"`
+}
+
+type SongList struct {
+    Songs []Song `json:"songs"`
+}
+
+func (sl *SongList) AddSong(s Song) *SongList {
+    sl.Songs = append(sl.Songs, s)
+    return sl
+}
+
+func (sl *SongList) Concat(ol *SongList) *SongList {
+    if ol == nil || ol.Songs == nil {
+        return sl
+    }
+    if sl.Songs == nil {
+        return ol
+    }
+    sl.Songs = append(sl.Songs, ol.Songs...)
+    return sl
+}
+
+func (sl *SongList) ToString() string {
+    jsonStr, err := json.Marshal(sl)
+    if err != nil {
+        log.Printf("error generating json string: %s", err)
+        return "error"
+    }
+    return string(jsonStr)
+}
+
+func GetUrl(client *http.Client, url string) []byte {
+    resp, err := client.Get(url)
+    if err != nil {
+        log.Printf("error get url %s: %s", url, err)
+        return nil
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Printf("error getting response body from url %s: %s", url, err)
+        return nil
+    }
+    return body
+}
+
 func main() {
+    log.Println(GetXiamiSong("1772292423").ToString())
 }
 
--- xiami_service.go
+++ xiami_service.go
@@ -1,3 +1,53 @@
 package main
 
+import (
+    "net/http"
+    "encoding/json"
+    "log"
+)
+
+const (
+    gXiamiProvider = "XiaMi Music Service"
+    gXiamiAPIUrlBase = "http://www.xiami.com/app"
+    gXiamiSongUrl = "/android/song/id/"
+    gXiamiAlbumUrl = "/iphone/album/id/"
+    gXiamiCollectUrl = "/android/collect?id="
+)
+
+var (
+    gXiamiClient = &http.Client{}
+)
+
+func GetXiamiSong(songId string) *Song {
+    url := gXiamiAPIUrlBase + gXiamiSongUrl + songId
+    ret := GetUrl(gXiamiClient, url)
+    if ret == nil {
+        return nil
+    }
+    var jsonmap map[string]json.RawMessage
+    err := json.Unmarshal(ret, &jsonmap)
+    if err != nil {
+        log.Printf("error parsing return data from url %s: %s", url, err)
+        return nil
+    }
+
+    var songmap map[string]string
+    err = json.Unmarshal(jsonmap["song"], &songmap)
+    if err != nil {
+        log.Printf("error parsing song info from url %s: %s", url, err)
+        return nil
+    }
+    return &Song{
+        Name: songmap["song_name"],
+        Url: songmap["song_location"],
+        Author: songmap["artist_name"],
+        Provider: gXiamiProvider,
+    }
+}
+
+func GetXiamiSongList(songs string) *SongList {
+}
+
+func GetXiamiCollect(collectId string) *SongList {
+}
 
